47ec202ef565571ad637767550555b57974de45b
Nishant kumar <nishantkumarfps@gmail.com>
2024-08-04 11:37:18 +0530

    feat: added auth form group

--- client/src/app/(estate)/(auth)/register/page.tsx
+++ client/src/app/(estate)/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
 // "use client";
-// import { AuthFormHeader, RegisterForm } from "@/components/forms/auth";
+import { AuthFormHeader } from "@/components/forms/auth";
 // import OauthButtons from "@/components/shared/OauthButtons";
 // import { useRedirectIfAuthenticated } from "@/hooks";
 
@@ -28,11 +28,15 @@
 // 	);
 // }
 
-
 export default function RegisterPage() {
 	return (
 		<div>
-			<h1 className="dark:text-pumpkin text-6xl">Register Page</h1>
+			<AuthFormHeader
+				title="Sign up for an account"
+				staticText="Already have an account?"
+				linkText="Login Here"
+				linkHref="/login"
+			/>
 		</div>
 	);
 }
