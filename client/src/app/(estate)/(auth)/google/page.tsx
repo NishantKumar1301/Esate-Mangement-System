cc34f201a2eaa15fac1ab152f85f264b7f97941d
Nishant kumar <nishantkumarfps@gmail.com>
2024-08-05 19:27:17 +0530

    feat : add signup and login with google

--- client/src/app/(estate)/(auth)/google/page.tsx
+++ client/src/app/(estate)/(auth)/google/page.tsx
@@ -1,36 +1,29 @@
-// "use client";
+"use client";
 
 import { randomFillSync } from "crypto";
 
-// import Spinner from "@/components/shared/Spinner";
-// import { useSocialAuth } from "@/hooks";
-// import { useSocialAuthenticationMutation } from "@/lib/redux/features/auth/authApiSlice";
-// import { Suspense } from "react";
+import Spinner from "@/components/shared/Spinner";
+import { useSocialAuth } from "@/hooks";
+import { useSocialAuthenticationMutation } from "@/lib/redux/features/auth/authApiSlice";
+import { Suspense } from "react";
 
-// export default function GoogleLoginPage() {
-// 	return (
-// 		<Suspense
-// 			fallback={
-// 				<div className="flex-center pt-32">
-// 					<Spinner size="xl" />
-// 				</div>
-// 			}
-// 		>
-// 			<GoogleLoginContent />
-// 		</Suspense>
-// 	);
-// }
-
-// function GoogleLoginContent() {
-// 	const [googleAuthenticate] = useSocialAuthenticationMutation();
-// 	useSocialAuth(googleAuthenticate, "google-oauth2");
-// 	return null;
-// }
-
-export default function GoogleLoginPage(){
-    return (
-        <div>
-            <h1 className="dark:text-pumpkin text-6xl">Google LogIn Page</h1>
-        </div>
-    )
+export default function GoogleLoginPage() {
+	return (
+		<Suspense
+			fallback={
+				<div className="flex-center pt-32">
+					<Spinner size="xl" />
+				</div>
+			}
+		>
+			<GoogleLoginContent />
+		</Suspense>
+	);
 }
+
+function GoogleLoginContent() {
+	const [googleAuthenticate] = useSocialAuthenticationMutation();
+	useSocialAuth(googleAuthenticate, "google-oauth2");
+	return null;
+}
+
