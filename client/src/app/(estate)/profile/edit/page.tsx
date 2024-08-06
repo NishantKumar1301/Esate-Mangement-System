6df07f653ab017ed07dcedd930d397338f5e4b8b
Nishant kumar <nishantkumarfps@gmail.com>
2024-08-06 11:18:17 +0530

    feat : added profile update page

--- client/src/app/(estate)/profile/edit/page.tsx
+++ client/src/app/(estate)/profile/edit/page.tsx
@@ -1,5 +1,5 @@
 import { AuthFormHeader } from "@/components/forms/auth";
-// import EditProfileForm from "@/components/forms/profile/EditProfileForm";
+import EditProfileForm from "@/components/forms/profile/EditProfileForm";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -14,7 +14,7 @@ export default function EditProfilePage() {
 			<div className="mt-7 sm:mx-auto sm:w-full sm:max-w-[480px]">
 				<div className="bg-lightGrey dark:bg-deepBlueGrey rounded-xl px-6 py-12 shadow sm:rounded-lg sm:px-12 md:rounded-3xl">
                     <h1 className="dark:text-babyPowder text-6xl">Edit Profile</h1>
-					{/* <EditProfileForm /> */}
+					<EditProfileForm />
 				</div>
 			</div>
 		</div>
